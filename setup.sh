0d019e7de7d4e3e32578797667180cd62371e958
ehanover <ephanover@gmail.com>
2022-05-13 14:54:43 -0400

    initial commit

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Enter this device's IP address (laptop): (without http:// or port)"
+echo "    (check using 'hostname -I')"
+echo "    (if running this in a VM, enter the host computer's IP)"
+read MyIP
+
+echo "Enter the robots's IP address (Jetson): (without http:// or port)"
+read RobotIP
+
+source /opt/ros/melodic/setup.bash
+
+if [ -f "devel_isolated/setup.bash" ]; then
+    # echo "Sourcing devel_isolated/setup.bash"
+    source devel_isolated/setup.bash
+elif [ -f "devel/setup.bash" ]; then
+    # echo "Sourcing devel/setup.bash"
+    source devel/setup.bash
+else
+    echo "Could not find a ROS setup.bash file! ROS commands may not work."
+fi
+
+
+export ROS_MASTER_URI=http://$RobotIP:11311
+export ROS_IP=$MyIP
+unset ROS_HOSTNAME # Setting both ROS_IP and ROS_HOSTNAME isn't necessary and it might cause problems if they're different
+
+echo Done.
